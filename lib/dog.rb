56ddb3a56e800f8fb8044b7a4b2b5f2644f1d605
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-10-20 17:48:45 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
-
+    #setting the name
+    def name= (dog_name)
+        @this_dogs_name = dog_name
+    end
+    #getting the name
+    def name
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
+
